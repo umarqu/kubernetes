587936fe664c72ab7a7b67d1d35886e372e43b22
umarqu <umar012894@gmail.com>
2024-11-27 16:52:39 +0000

    first commit

---
+++ docker_base_images/java/Hello.java
@@ -0,0 +1,9 @@
+package java;
+
+class Hello{
+    public static void main(String [] args){
+        System.out.println("Hello Docker");
+    }
+}
+
+// the docker file is not precompiled. here we will compile it and then run it
